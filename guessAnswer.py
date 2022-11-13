2796e51dcfac8f94701055435d4cab0eaad52ead
Your Name <you@example.com>
2022-11-13 13:09:46 +0800

    新增例外

--- guessAnswer.py
+++ guessAnswer.py
@@ -9,3 +9,26 @@ while ans != guess:
         min = guess
         print("太小了")
 print("讚啦")
+
+import random
+from random import randint as rdt
+guess , ans = 0, rdt(1,100)
+l , h = 0 , 100
+while ans != guess:
+    try:
+        guess = int(input(str(l)+"~"+str(h)+">>"))
+    except:
+        print("請輸入正確的數字")
+        continue
+    if guess < l or guess > h:
+        print("請輸入正確區間的數字")
+        continue
+    elif ans < guess:
+        h = guess
+        print("太大了")
+    elif ans > guess:
+        l = guess
+        print("太小了")
+    elif ans == guess:
+        break
+print("恭喜")
